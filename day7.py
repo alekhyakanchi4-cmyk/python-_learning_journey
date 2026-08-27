4e62f41d37f5b8a1d4ae67136e5c68012fd7777f
alekhyakanchi4-cmyk <alekhyakanchi4@gmail.com>
2026-08-27 21:05:15 +0530

    day7-while loop

---
+++ day7.py
@@ -0,0 +1,86 @@
+#27/8
+'''n=input()
+for i in n :
+    print(i)'''
+#to print all numbers divisible by both x abd y
+'''a=int(input())
+b=int(input())
+x=int(input())
+y=int(input())
+for i in range(a,b+1):
+    if i%x==0 and i%y==0:
+        print(i)'''
+#printing count from given input ex:13 o/p=2
+'''x=input()
+count=0
+for i in x:
+    count+=1
+print(count)'''
+
+#sum of given numbers
+'''x=input()
+sum=0
+for i in x:
+    sum=sum+int(i)
+print(sum)'''
+#table
+'''x=int(input())
+for i in range(1,11):
+    print(f"{x}x{i:2d}={x*i:2d}")'''
+    
+#print("%dx%2d=%2d"(n,i,n*i)
+#while
+'''i=1
+while i<=5:
+    print(i)
+    i=i+1'''
+
+#even
+'''i=1
+while i<=30:
+    if i%2==0:
+        print(i)
+    i=i+1'''
+#odd
+'''i=1
+while i<=30:
+    if i%2!=0:
+        print(i)
+    i=i+1'''
+#numbers reverse order
+'''     
+i=5
+while i>=1:
+    print(i)
+    i=i-1'''
+
+
+#reverse a number
+'''i=int(input())
+rev=0
+while i!=0:
+    a=(i//100)%10
+    b=(i//10)%10
+    c=i%10
+    print(c*100+b*10+a)
+    i=0'''
+
+#or
+
+'''n=int(input())
+rev=0
+while n!=0:
+    digit=n%10
+    rev=rev*10+digit
+    n=n//10
+print(rev)'''
+    
+    
+    
+    
+    
+
+
+
+
+
