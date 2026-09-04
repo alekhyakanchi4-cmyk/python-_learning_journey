7839f6c939c673044f691bdcc726554ab2e71f60
alekhyakanchi4-cmyk <alekhyakanchi4@gmail.com>
2026-09-04 21:48:08 +0530

    day_12 strings

---
+++ day12.py
@@ -0,0 +1,58 @@
+#strings
+'''s="sreevahini"
+print(s[0])
+print(s[9])
+print(s[4])
+print(s[-1])
+print(s[-10])
+print(s[-6])'''
+
+'''#slicing
+s="javaprogramming"
+print(s[0:4])
+#no ending value
+print(s[0:])
+#no starting value
+print(s[:4])'''
+
+
+
+'''s="python programming"
+print(s[7:18])
+print(s[0:6])
+print(s[7:11])
+print(s[10:18])'''
+
+
+'''s="python programming"
+print(s[-13:11])
+print(s[-7:7:-1])
+print(s[3:-9])
+print(s[11:-16])
+print(s[11:-16:-1])'''
+
+'''s="python programming"
+print(s[-13:-9])
+print(s[-8:-1])
+print(s[-8:-18:-1])
+print(s[::-1])
+print(s[0:6])
+print(s[5::-1])
+print(s[5::-1]+s[7:])
+print(s[0:6]+s[7:])
+print(s[5::-1]+s[-1:-12:-1])
+print(s[5::-1]+ " "+s[-1:-12:-1])'''
+
+
+'''s="python"
+for i in s:
+    print(i)
+
+for i in range(len(s)):
+    print(s[i],end=" ")'''
+    
+
+
+
+
+
