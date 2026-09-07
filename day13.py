50f57910901211768ad46163bebfc2bbf577b280
alekhyakanchi4-cmyk <alekhyakanchi4@gmail.com>
2026-09-07 20:39:36 +0530

    Add files via upload

---
+++ day13.py
@@ -0,0 +1,44 @@
+
+#count of even numbers
+'''n=int(input())
+if  n>=0:
+    c=0
+    for i  in str(n):
+        if int(i)%2==0:
+            c=c+1
+    print(c)
+else:
+    print("invalid number")'''
+
+
+'''s=input()
+res=""
+for i in s:
+    ch=i
+    if ch not in res:
+        res=res+ch
+        count=0
+        for i in s:
+            if i==ch:
+                count=count+1
+        print(ch+str(count),end=" ")'''
+
+
+
+
+'''print(None and True)'''#none
+'''print(5==5==True)'''  #false
+
+print(not True==False)
+
+print(1 and 0 or 1 and 0)
+
+
+
+
+
+
+
+
+
+
